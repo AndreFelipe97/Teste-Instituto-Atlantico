29675437a1d994b6bae8876e918d94f06d593f9e
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-07-29 22:43:06 -0300

    Footer styling

---
+++ frontend/frontend/src/component/_Layout/Default/FooterComponent/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import { Layout } from 'antd';
+
+export const Footer = styled(Layout.Footer)`
+  display: flex;
+  justify-content: space-between;
+  color: #fff;
+  background: rgba(45, 166, 109, 1);
+`;
