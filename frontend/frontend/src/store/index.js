6ae5973296f0297f7f3d1b2a1e71da9813118eb5
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-07-29 22:57:17 -0300

    Configuring access to redux and redux saga

---
+++ frontend/frontend/src/store/index.js
@@ -0,0 +1,19 @@
+import { persistStore } from 'redux-persist';
+import createSagaMiddleware from 'redux-saga';
+import createStore from './createStore';
+import persistReducer from './persistReducer';
+
+import rootReducer from './module/rootReducer';
+import rootSaga from './module/rootSaga';
+
+const sagaMonitor = process.env.NODE_ENV === 'development' ? console.tron.createSagaMonitor() : null;
+
+const sagaMiddleware = createSagaMiddleware({ sagaMonitor })
+const middlwares = [sagaMiddleware]
+
+const store = createStore(persistReducer(rootReducer), middlwares);
+const persistor = persistStore(store);
+
+sagaMiddleware.run(rootSaga);
+
+export { store, persistor };
