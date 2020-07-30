f4acd89b4ed560c7dc385a2dc3487a69a09d2431
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-07-29 22:51:11 -0300

    Stylization of the firmware form

---
+++ frontend/frontend/src/pages/FileRegistration/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import { Button } from 'antd';
+
+export const ButtonSubmit = styled(Button)`
+  background: #06a850;
+  color: #fff;
+  border-radius: 10px;
+
+  &:hover {
+    background: #06a850 !important;
+    color: #fff !important;
+    border-color: transparent !important;
+  }
+`;
