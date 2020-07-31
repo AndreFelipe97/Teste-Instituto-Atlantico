dba25cfd9578ca5c63d665ad4cd6723b86747dbf
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-07-31 11:27:30 -0300

    Validation has been placed for registration

--- backend/backend/src/app/controllers/FileController.js
+++ backend/backend/src/app/controllers/FileController.js
@@ -4,11 +4,15 @@ class FileController {
   async store(request, response) {
     const { originalname: name, filename: path } = request.file;
 
+    if (!request.userUpload_file) {
+      return response.status(400).json({ error: 'User does not have permission' });
+    }
+
     const file = await File.create({
       name,
       path,
     });
-    
+
     return response.status(200).json(file);
   }
 }
