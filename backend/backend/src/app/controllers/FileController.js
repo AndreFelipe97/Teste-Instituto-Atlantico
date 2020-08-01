b27c917153c83bf04289c9e53c26fd435f6f952e
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-08-01 17:32:37 -0300

    The string Format function was imported, to format the file name before saving

--- backend/backend/src/app/controllers/FileController.js
+++ backend/backend/src/app/controllers/FileController.js
@@ -1,8 +1,10 @@
 import File from '../models/File';
+import stringFormat from '../../util/stringFormat';
 
 class FileController {
   async store(request, response) {
-    const { originalname: name, filename: path } = request.file;
+    const { originalname, filename: path } = request.file;
+    const name = stringFormat(originalname, request.body.project, request.body.version);
 
     if (!request.userUpload_file) {
       return response.status(400).json({ error: 'User does not have permission' });
